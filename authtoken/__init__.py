6670de4d5aa1b84fba16ea2db02ff2be11c33133
scign <scignspam.github@mailnull.com>
2021-02-20 22:58:01 -0500

    added zoom token

--- authtoken/__init__.py
+++ authtoken/__init__.py
@@ -1,59 +1,10 @@
-import os
-import jwt
-import requests
 import logging
-from datetime import datetime, timedelta
+from authtoken import get_youtube_token, get_zoom_token
 import azure.functions as func
 
-
 """
-Must be deployed WITH authentication (i.e. function key)
+Must be deployed WITH authentication (i.e. function keys)
 """
-
-def get_youtube_token(full_response=False):
-    logging.info('Attempting to get YouTube token')
-    private_key = os.environ.get('PRIVATE_KEY','').encode('utf-8')
-    svc_account = os.environ.get('SERVICE_ACCOUNT','')
-    token_uri = 'https://oauth2.googleapis.com/token'
-    iat = datetime.utcnow()
-    exp = iat + timedelta(seconds=3600)
-    jwt_headers = {'alg':'RS256','typ':'JWT'}
-    scopes = [
-        'https://www.googleapis.com/auth/youtube',
-        'https://www.googleapis.com/auth/youtube.readonly',
-        'https://www.googleapis.com/auth/youtubepartner',
-        'https://www.googleapis.com/auth/yt-analytics.readonly',
-        'https://www.googleapis.com/auth/yt-analytics-monetary.readonly',
-    ]
-    jwt_claim = {
-        'iss':svc_account,
-        'scope':' '.join(scopes),
-        'aud':token_uri,
-        'exp':exp,
-        'iat':iat
-    }
-    encoded = jwt.encode(jwt_claim, private_key, algorithm='RS256', headers=jwt_headers)
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    r_token = requests.post(
-        token_uri,
-        data='grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer' + '&assertion=' + encoded,
-        headers=headers
-    )
-    if full_response:
-        return r_token
-    else:
-        logging.info(f'Sent JWT token, received status code {r_token.status_code}')
-        if r_token.status_code==200:
-            logging.info(f"Access token generated at {iat.isoformat()} for expiry at {exp.isoformat()}")
-            logging.info(f"Access token ({r_token.json()['token_type']}) expires in {r_token.json()['expires_in']} seconds")
-            return r_token.json()['access_token']
-        else:
-            logging.error(f"Access token request failed with status code {r_token.status_code}")
-            try:
-                logging.info(f"Request response JSON: {r_token.json()}")
-            except:
-                pass
-            return ''
 
 def main(req: func.HttpRequest) -> str:
     """
@@ -73,10 +24,15 @@ def main(req: func.HttpRequest) -> str:
         else:
             name = req_body.get('name')
 
-    if name=='youtube':
-        token = get_youtube_token()
+    token_functions = {
+        'youtube': get_youtube_token,
+        'zoom': get_zoom_token
+    }
+    try:
+        token_func = token_functions.get(name)
+        token = token_func()
         return func.HttpResponse(token)
-    else:
+    except KeyError:
         return func.HttpResponse(
              "Pass a name in the query string or in the request body for a token.",
              status_code=400
---
+++ authtoken/auth.py
@@ -0,0 +1,67 @@
+import os
+import jwt
+import requests
+import logging
+from datetime import datetime, timedelta
+
+def get_jwt(issuer, scopes = [], uri='', key='', algo='RS256'):
+    iat = datetime.utcnow()
+    exp = iat + timedelta(seconds=3600)
+    jwt_headers = {'alg':algo,'typ':'JWT'}
+    jwt_claim = {
+        'iss':issuer,
+        'scope':' '.join(scopes),
+        'aud':uri,
+        'exp':exp,
+        'iat':iat
+    }
+    logging.info(f"Generating JWT at {iat.isoformat()} for expiry at {exp.isoformat()}")
+    return jwt.encode(jwt_claim, key, algorithm=algo, headers=jwt_headers)
+
+def get_youtube_token(full_response=False):
+    logging.info('Attempting to get YouTube token')
+    token_uri = 'https://oauth2.googleapis.com/token'
+    svc_account = os.environ.get('GOOGLE_SERVICE_ACCOUNT','')
+    private_key = os.environ.get('GOOGLE_PRIVATE_KEY','').encode('utf-8')
+    scopes = [
+        'https://www.googleapis.com/auth/youtube',
+        'https://www.googleapis.com/auth/youtube.readonly',
+        'https://www.googleapis.com/auth/youtubepartner',
+        'https://www.googleapis.com/auth/yt-analytics.readonly',
+        'https://www.googleapis.com/auth/yt-analytics-monetary.readonly',
+    ]
+    myjwt = get_jwt(
+        issuer=svc_account,
+        scopes=scopes,
+        uri=token_uri,
+        key=private_key
+    )
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    r_token = requests.post(
+        token_uri,
+        data='grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer' + '&assertion=' + myjwt,
+        headers=headers
+    )
+    if full_response:
+        return r_token
+    else:
+        logging.info(f'Sent JWT token, received status code {r_token.status_code}')
+        if r_token.status_code==200:
+            logging.info(f"Access token ({r_token.json()['token_type']}) expires in {r_token.json()['expires_in']} seconds")
+            return r_token.json()['access_token']
+        else:
+            logging.error(f"Access token request failed with status code {r_token.status_code}")
+            try:
+                logging.info(f"Request response JSON: {r_token.json()}")
+            except:
+                pass
+            return ''
+
+def get_zoom_token(full_response=False):
+    myjwt = get_jwt(
+        issuer=os.environ.get('ZOOM_API_KEY',''),
+        key=os.environ.get('ZOOM_API_SECRET','').encode('utf-8'),
+        algo='HS256'
+    )
+    return myjwt
+
